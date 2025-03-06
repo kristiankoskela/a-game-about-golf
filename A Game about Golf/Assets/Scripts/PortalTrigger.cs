e575557b004483e0dc2a31c1b4903de90a2c4916
koskela-laptop <kristian.koskela@proton.me>
2025-03-06 10:23:16 +0200

    portal sfx

--- A Game about Golf/Assets/Scripts/PortalTrigger.cs
+++ A Game about Golf/Assets/Scripts/PortalTrigger.cs
@@ -4,10 +4,17 @@ public class PortalTrigger : MonoBehaviour
 {
     public GameObject outPortal;
 
+    public AudioClip[] portalArray;
+    public float portalVolume;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
+            int randomIndex = Random.Range(0, portalArray.Length);
+            SoundManager.instance.PlaySoundClip(portalArray[randomIndex], transform, portalVolume);
+
             Rigidbody rb = other.GetComponent<Rigidbody>();
             if (rb != null)
             {
